b7ef95e87e4578bc6a3f2da8418564829b81534e
Tikkaaa3 <tikkaaa3@gmail.com>
2025-04-27 22:15:08 +0200

    Player class updated

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import pygame
 
-from constants import (
-    ASTEROID_KINDS,
-    ASTEROID_MAX_RADIUS,
-    ASTEROID_MIN_RADIUS,
-    ASTEROID_SPAWN_RATE,
-    SCREEN_HEIGHT,
-    SCREEN_WIDTH,
-)
+from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -22,6 +20,11 @@ def main():
                 return
 
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        updatable.update(dt)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
